2d13fc3a435f30f32e8e37734086a13bf60feef8
jahdielmcuenta2-arch <jahdielmcuenta2@gmail.com>
2026-02-08 17:21:43 -0500

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -8,6 +8,8 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_STORAGE_BUCKET: string;
   readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
   readonly VITE_FIREBASE_APP_ID: string;
+  // PIN de seguridad global (4 dígitos)
+  readonly VITE_SECURITY_PIN: string;
 }
 
 interface ImportMeta {
